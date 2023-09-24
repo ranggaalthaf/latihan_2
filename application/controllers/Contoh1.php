7ee26e79dc6bf25eb6c421da34ab979799f66d65
adityasatrianugraha <adityasatria306@gmail.com>
2023-09-25 00:50:01 +0700

    Menambahkan berkas index.html, style.css, dan script.js

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalan </h1>";
+        echo "<h1> Nama Saya Aditya Satria Nugraha
+                   Saya tinggal didaerah kemanggisan
+                   Sepak bola adalah olahraga kesukaan saya</h1>";
+        
+    }
+}
