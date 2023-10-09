8c02a2147f30b3607e484bc9199891d450eeaf31
ranggaalthaf <ranggaalthaf28@gmail.com>
2023-10-09 16:35:28 +0700

    telah di tambahkan oleh rangga

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rangga</li>
+        <li>Nama Belakang : Althaf</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Tanah Baru V </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Mini Soccer</li>
+    <li>Futsal</li>
+    </section>
